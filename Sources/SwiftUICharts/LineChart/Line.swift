9c14f4153a23da73b1828967982df70907a977c8
Marian Arsul <marian.arsul00@e-uvt.ro>
2022-06-11 17:23:49 +0300

    Update Line.swift

--- Sources/SwiftUICharts/LineChart/Line.swift
+++ Sources/SwiftUICharts/LineChart/Line.swift
@@ -17,7 +17,7 @@ public struct Line: View {
     @Binding var maxDataValue: Double?
     @State private var showFull: Bool = false
     @State var showBackground: Bool = true
-    @State var shouldAnimate: Bool?
+    @State var shouldAnimate: Bool = false
     var gradient: GradientColor = GradientColor(start: Colors.GradientPurple, end: Colors.GradientNeonBlue)
     var index:Int = 0
     let padding:CGFloat = 30
